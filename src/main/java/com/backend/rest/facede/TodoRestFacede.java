55dcec96d71515d58bd58c0202ea807f83a81f29
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-06-14 23:25:47 -0300

    Projeto pronto para bateria de testes

--- src/main/java/com/backend/rest/facede/TodoRestFacede.java
+++ src/main/java/com/backend/rest/facede/TodoRestFacede.java
@@ -2,6 +2,7 @@ package com.backend.rest.facede;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -19,40 +20,56 @@ import com.backend.model.TodoModel;
 @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 @Consumes(MediaType.APPLICATION_JSON)
 public class TodoRestFacede {
-	static int id = 0;
-	private boolean markAllTodo = false;
+
+	private static int id = 0;
 	
 	private static List<TodoModel> todoList = new ArrayList<TodoModel>();
 
 	@GET
 	public List<TodoModel> getTodos() {
+	
 		return todoList;
+	
 	}
 
 	@POST
 	public TodoModel salvarTodo(String newTodo) {
+	
 		TodoModel novoTodo = new TodoModel(id, newTodo, false);
+		
 		todoList.add(novoTodo);
 		id++;
+		
 		return novoTodo;
+	
 	}
 
 	@PUT
-	public void atualizarStatus(TodoModel todo) {
-		System.out.println(todo.getId());
-		if(todoList.get(todoList.indexOf(todo)).getTodo().equals(todo.getTodo())){
-			boolean val = todoList.get(todoList.indexOf(todo)).isStatus();
-			todoList.get(todoList.indexOf(todo)).setStatus(!val);
+	public void atualizarTodo(TodoModel todo) {
+	
+		int index = todoList.indexOf(todo);
+		String todoLista = todoList.get(index).getTodo(); 
+		
+		if(todoLista.equals(todo.getTodo())){
+		
+			boolean status = todoList.get(index).isStatus();
+		
+			todoList.get(index).setStatus(!status);
+	
 		}else {
-			todoList.get(todoList.indexOf(todo)).setTodo(todo.getTodo());
+		
+			todoList.get(index).setTodo(todo.getTodo());
+		
 		}
+		
 	}
 	
 	@PUT
 	@Path("/allChecked/{allChecked}")
 	public void markAllTodo(@PathParam("allChecked") Boolean status) {
-		System.out.println("entrou aqui " + status);
+		
 		for(TodoModel todo : todoList) {
+		
 			todo.setStatus(status);
 		}
 	}
@@ -60,19 +77,29 @@ public class TodoRestFacede {
 	@DELETE
 	@Path("/{idTodo}")
 	public void executaDELETE(@PathParam("idTodo") Integer idTodo) {
+		
 		TodoModel todo = new TodoModel();
+		
 		todo.setId(idTodo);
 		todoList.remove(todo);
+	
 	}
 
 	@DELETE
 	public void executaDELETECompleted() {
+	
 		List<TodoModel> listDelete = new ArrayList<TodoModel>();
+		
 		for (TodoModel todo : todoList) {
+		
 			if (todo.isStatus()) {
+				
 				listDelete.add(todo);
+			
 			}
+			
 		}
+		
 		todoList.removeAll(listDelete);
 	}
 
--- src/main/webapp/controller/todo.js
+++ src/main/webapp/controller/todo.js
@@ -1,4 +1,4 @@
-var todoModulo = angular.module('todoModulo', []);
+todoModulo = angular.module('todoModulo', []);
 
 todoModulo.controller("todoController", function($scope, $http, $filter) {
 
@@ -8,7 +8,7 @@ todoModulo.controller("todoController", function($scope, $http, $filter) {
 	var flagActives = false;
 	var flagCompletes = false;
 	var flagshowAll = false;
-	//var listTodos;
+	var flagReverse = false;
 
 	//SELECT
 	$scope.showAll = function() {
@@ -17,164 +17,280 @@ todoModulo.controller("todoController", function($scope, $http, $filter) {
 		document.getElementById('toggle-all').checked = $scope.allChecked = true;
 
 		function sucessCallback(todos) {
+
 			$scope.listTodos = $scope.todos = todos.data;
 			$scope.remainingCount = $filter('filter')($scope.todos, { status: false }).length;
 
 			if ($scope.remainingCount == $scope.todos.length) {
+
 				$scope.completedCount = false;
+
 			} else {
+
 				$scope.completedCount = true;
+
 			}
 
 			angular.forEach($scope.todos, function(verificaStatus, key) {
+
 				if (verificaStatus.status == false) {
+
 					document.getElementById('toggle-all').checked = $scope.allChecked = false;
 					keepGoing = false;
+
 				}
 			});
-			
+
 			if (flagCompletes) {
+
 				$scope.showCompletedTodos();
+
 			} else if (flagActives) {
+
 				$scope.showActiveTodos();
+
 			} else if (flagshowAll) {
+
 				$scope.showAllTodos();
+
 			}
 
 		}
 
 		function errorCalback(error) {
+
 			alert(error);
+
 		}
 
 	}
 
 	//INSERT
 	$scope.addTodo = function() {
-		if (!($scope.newTodo == undefined)) {
+
+		if ( $scope.newTodo != undefined && $scope.newTodo.trim()) {
+
 			$http.post(urlTodo, $scope.newTodo).then(sucessCallback, errorCalback);
 
 			function sucessCallback() {
+
 				$scope.saving = true;
+
 				if ($scope.remainingCount == $scope.todos.length) {
+
 					$scope.completedCount = false;
+
 				} else {
+
 					$scope.completedCount = true;
+
 				}
+
 				$scope.showAll();
 				$scope.newTodo = null;
 				$scope.saving = false;
+
 			}
 
+
+
 			function errorCalback(error) {
+
 				alert(error);
+
 			}
 
 		}
 	}
 
 	//UPDATE
-	$scope.todoCompleted = function(todo) {
+	$scope.updateTodo = function(todo) {
+
 		$http.put(urlTodo, todo).then(sucessCallback, errorCalback);
 
 		function sucessCallback() {
+
 			$scope.showAll();
+
 		}
 
 		function errorCalback(error) {
+
 			alert(error);
+
 		}
 
 	}
 
 	$scope.markAll = function() {
+
 		$scope.allChecked = !($scope.allChecked);
 
 		$http.put(urlTodo + '/allChecked/' + $scope.allChecked).then(sucessCallback, errorCalback);
 
 		function sucessCallback() {
+
 			document.getElementById('toggle-all').checked = $scope.allChecked;
-			$scope.showAllTodos();
 			$scope.showAll();
+
 		}
 
 		function errorCalback(error) {
+
 			alert(error);
-		}
 
-	}
-
-	$scope.saveEdits = function(updatedTodo, event) {
-
-
-		if (event === 'blur' && $scope.saveEvent === 'submit') {
-			$scope.saveEvent = null;
-			return;
-		}
-
-		$scope.saveEvent = event;
-		if (todoName == updatedTodo.todo || updatedTodo.todo == '') {
-			$scope.showAll();
-		} else {
-			$scope.todoCompleted(updatedTodo);
 		}
 
 	}
 
 	//REMOVE
 	$scope.clearCompleted = function() {
+
 		$http.delete(urlTodo).then(sucessCallback, errorCalback);
 
 		function sucessCallback() {
-			$scope.listTodos = null;
+
+			if (((($scope.listTodos.length - $scope.todos.length) == 0) || ($scope.listTodos.length - $scope.todos.length) == $scope.listTodos.length)) {
+
+				$scope.showAllTodos();
+
+			}
+
 			$scope.showAll();
+
 		}
 
 		function errorCalback(error) {
+
 			alert(error);
+
 		}
 	}
 
 	$scope.removeTodo = function(todo) {
+
 		$http.delete(urlTodo + '/' + todo.id).then(sucessCallback, errorCalback);
 
 		function sucessCallback() {
+
+
+			if (($scope.listTodos.length) == 1) {
+				$scope.showAllTodos();
+			}
+
 			$scope.showAll();
+
 		}
 
 		function errorCalback(error) {
+
 			alert(error);
+
 		}
+
 	}
 
 	//FUNCTIONS
 
 	$scope.editTodo = function(todo) {
+
 		$scope.editedTodo = todo;
 		todoName = todo.todo;
 	}
 
+	$scope.revertEdits = function(todo) {
+
+		flagReverse = true;
+		$scope.saveEdits(todo, 'submit');
+		flagReverse = false;
+
+	}
+
+	$scope.saveEdits = function(updatedTodo, event) {
+
+		if (!(flagReverse)) {
+			if (updatedTodo.todo == '' || !updatedTodo.todo.trim()) {
+				$scope.removeTodo(updatedTodo);
+			}else if ((todoName != updatedTodo.todo)) {
+				$scope.updateTodo(updatedTodo);
+			}
+		}
+
+		$scope.showAll();
+
+	}
+
+
 	$scope.showAllTodos = function() {
+
 		$scope.todos = $scope.listTodos;
 		flagshowAll = true;
 		flagActives = false;
 		flagCompletes = false;
+		$scope.selectedShowAll = true;
+		$scope.selectedShowActive = false;
+		$scope.selectedShowCompleted = false;
+
 	}
 
 	$scope.showActiveTodos = function() {
+
 		$scope.todos = $filter('filter')($scope.listTodos, { status: false });
 		flagshowAll = false;
 		flagActives = true;
 		flagCompletes = false;
+		$scope.selectedShowAll = false;
+		$scope.selectedShowActive = true;
+		$scope.selectedShowCompleted = false;
+
 	}
 
 	$scope.showCompletedTodos = function() {
+
 		$scope.todos = $filter('filter')($scope.listTodos, { status: true });
 		flagshowAll = false;
 		flagActives = false;
 		flagCompletes = true;
+		$scope.selectedShowAll = false;
+		$scope.selectedShowActive = false;
+		$scope.selectedShowCompleted = true;
+
 	}
+
 
 	$scope.showAll();
 
+});
+
+todoModulo.directive('todoEscape', function() {
+
+	'use strict';
+
+	var ESCAPE_KEY = 27;
+
+	return function(scope, elem, attrs) {
+		elem.bind('keydown', function(event) {
+			if (event.keyCode === ESCAPE_KEY) {
+				scope.$apply(attrs.todoEscape);
+			}
+		});
+
+		scope.$on('$destroy', function() {
+			elem.unbind('keydown');
+		});
+	};
+});
+
+todoModulo.directive('todoFocus', function todoFocus($timeout) {
+	'use strict';
+
+	return function(scope, elem, attrs) {
+		scope.$watch(attrs.todoFocus, function(newVal) {
+			if (newVal) {
+				$timeout(function() {
+					elem[0].focus();
+				}, 0, false);
+			}
+		});
+	};
 });
